f52439482b9d7d50f72fd8d467c2d99b514e2572
Philip Yu <philipyu1997@gmail.com>
2020-07-14 13:25:42 -0700

    Update .gitignore

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -67,7 +67,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text(
